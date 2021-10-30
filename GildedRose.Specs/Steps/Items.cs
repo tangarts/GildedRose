0bd5dd731e781bc0bd72f64979bd60d88dd13a09
Nehemiah Campbell <nehemiah.campbell@endjin.com>
2021-10-30 19:54:39 +0100

    move item to separate file

---
+++ GildedRose.Specs/Steps/Items.cs
@@ -0,0 +1,49 @@
+﻿using TechTalk.SpecFlow;
+
+namespace GildedRose.Specs.Steps
+{
+    [Binding]
+    public sealed class ItemsStepDefinitions
+    {
+
+        private readonly ScenarioContext _scenarioContext;
+
+        public ItemsStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given(@"I have an item with the name of ""(.*)""")]
+        public void GivenIHaveAnItemWithTheNameOf(string p0)
+        {
+            _scenarioContext.Pending();
+        }
+
+        [Given(@"I have an item")]
+        public void GivenIHaveAnItem()
+        {
+            _scenarioContext.Pending();
+        }
+
+        [When(@"the sell in value decreases by (.*)")]
+        public void WhenTheSellInValueDecreasesBy(int p0)
+        {
+            _scenarioContext.Pending();
+        }
+
+
+        [Then(@"the quality should increase by (.*)")]
+        public void ThenTheQualityShouldIncreaseBy(int p0)
+        {
+            _scenarioContext.Pending();
+        }
+
+        [Then(@"the quality should be (.*)")]
+        public void ThenTheQualityShouldBe(int p0)
+        {
+            _scenarioContext.Pending();
+        }
+
+
+    }
+}
