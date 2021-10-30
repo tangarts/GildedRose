884b838412d835d9153e53d56f1657bd2400e619
Nehemiah Campbell <nehemiah.campbell@endjin.com>
2021-10-30 18:50:14 +0100

    clarify formatting

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -13,25 +13,25 @@ namespace GildedRose.Console
             var app = new Program()
             {
                 Items = new List<Item>
-                                          {
-                                              new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                                              new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                                              new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                                              new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                                              new Item
-                                                  {
-                                                      Name = "Backstage passes to a TAFKAL80ETC concert",
-                                                      SellIn = 15,
-                                                      Quality = 20
-                                                  },
-                                              new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-                                          }
+                {
+                    new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                    new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                    new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                    new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                    new Item
+                        {
+                            Name = "Backstage passes to a TAFKAL80ETC concert",
+                            SellIn = 15,
+                            Quality = 20
+                        },
+                    new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                }
 
             };
 
             app.UpdateQuality();
 
-            System.Console.ReadKey();
+            //System.Console.ReadKey();
 
         }
 
@@ -45,7 +45,7 @@ namespace GildedRose.Console
                     {
                         if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
                         {
-                            Items[i].Quality = Items[i].Quality - 1;
+                            Items[i].Quality -= 1;
                         }
                     }
                 }
@@ -53,7 +53,7 @@ namespace GildedRose.Console
                 {
                     if (Items[i].Quality < 50)
                     {
-                        Items[i].Quality = Items[i].Quality + 1;
+                        Items[i].Quality += 1;
 
                         if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
                         {
@@ -61,7 +61,7 @@ namespace GildedRose.Console
                             {
                                 if (Items[i].Quality < 50)
                                 {
-                                    Items[i].Quality = Items[i].Quality + 1;
+                                    Items[i].Quality += 1;
                                 }
                             }
 
@@ -69,7 +69,7 @@ namespace GildedRose.Console
                             {
                                 if (Items[i].Quality < 50)
                                 {
-                                    Items[i].Quality = Items[i].Quality + 1;
+                                    Items[i].Quality += 1;
                                 }
                             }
                         }
@@ -78,7 +78,7 @@ namespace GildedRose.Console
 
                 if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
                 {
-                    Items[i].SellIn = Items[i].SellIn - 1;
+                    Items[i].SellIn -= 1;
                 }
 
                 if (Items[i].SellIn < 0)
@@ -91,7 +91,7 @@ namespace GildedRose.Console
                             {
                                 if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
                                 {
-                                    Items[i].Quality = Items[i].Quality - 1;
+                                    Items[i].Quality -= 1;
                                 }
                             }
                         }
@@ -104,22 +104,13 @@ namespace GildedRose.Console
                     {
                         if (Items[i].Quality < 50)
                         {
-                            Items[i].Quality = Items[i].Quality + 1;
+                            Items[i].Quality += 1;
                         }
                     }
                 }
             }
         }
 
-    }
-
-    public class Item
-    {
-        public string Name { get; set; }
-
-        public int SellIn { get; set; }
-
-        public int Quality { get; set; }
     }
 
 }
